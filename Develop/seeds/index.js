52a1dae6ab7383ebf015e9e7c8edd252f2f12f25
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-10-19 23:51:40 -0400

    converted to ES module syntax (import)

--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -1,9 +1,9 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+import seedCategories from './category-seeds';
+import seedProducts from './product-seeds';
+import seedTags from './tag-seeds';
+import seedProductTags from './product-tag-seeds';
 
-const sequelize = require('../config/connection');
+import sequelize from '../config/connection';
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
